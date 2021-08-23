04863dc8e9d0f0503fecd587b561c8eacb67b65f
RubenCastillo1 <A01721993@itesm.mx>
2021-08-22 21:13:57 -0500

    01Promedio A01721993

--- assignments/01Promedio/src/exercise.py
+++ assignments/01Promedio/src/exercise.py
@@ -1,6 +1,16 @@
+from typing import Counter
+
+
 def main():
     #escribe tu código abajo de esta línea
-    pass
+   c1 = float(input("dame la calificacion 1: "))
+   c2 = float(input("dame la calificacion 2: "))
+   c3 = float(input("dame la calificacion 3: "))
+   c4 = float(input("dame la calificacion 4: "))
+   pro = (c1+c2+c3+c4)/4
+   print("El promedio es: "+str(pro))
+
+
 
 
 if __name__ == '__main__':
